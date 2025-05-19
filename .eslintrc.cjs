f7350c6ed7a2946c77697df0e9f107ee37f94c15
Rostyslav Futornyi <ros@Rostyslavs-MacBook-Pro.local>
2025-05-19 22:53:51 +0300

    [SUP-74]: fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -34,7 +34,6 @@ module.exports = {
     'react/require-default-props': 'off',
     'prettier/prettier': 'error',
     'no-console': ['warn', { allow: ['warn', 'error'] }],
-    'no-unused-vars': 'off',
   },
   globals: {
     describe: 'readonly',
